7150db37a5169dc08c2d9d917d62985e7d87a0cf
jonathand <jonathand@jonathand-mbp.jnpr.net>
2020-07-22 14:59:51 -0400

    removed unnecessary prints

--- RedisClient.cpp
+++ RedisClient.cpp
@@ -49,9 +49,6 @@ double RedisClient::run_threads(Lambda f, int begin, bool random, int n)
 
     int runs = DBClient::getRuns();
 
-    cout << "Threads: " << threads << endl;
-    cout << "Runs: " << runs << endl;
-
     int numOfRuns = (n == 0) ? runs : n;
 
     int perThread = numOfRuns / threads;
